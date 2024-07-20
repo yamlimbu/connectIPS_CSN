e4ed1b4cd0d93043ab49d0fb219225300b62caa9
Limbu <yam.limbu@dryicesolutions.net>
2024-07-20 08:56:58 +0545

    design merged and fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,28 +1,4 @@
 <header class="header_section">
-    {{-- <div class="header_top">
-          <div class="container">
-              <div class="contact_nav">
-                  <a href="">
-                      <i class="fa fa-phone" aria-hidden="true"></i>
-                      <span>
-                          Call : +01 123455678990
-                      </span>
-                  </a>
-                  <a href="">
-                      <i class="fa fa-envelope" aria-hidden="true"></i>
-                      <span>
-                          Email : demo@gmail.com
-                      </span>
-                  </a>
-                  <a href="">
-                      <i class="fa fa-map-marker" aria-hidden="true"></i>
-                      <span>
-                          Location
-                      </span>
-                  </a>
-              </div>
-          </div>
-      </div> --}}
     <div class="header-top text-center text-white">
         <div class="container d-flex justify-content-center align-items-center gap-3">
             <div>Join us for Management Of Cardiovascular Disease 2024, October 25-26 in Kathmandu, Nepal.</div>
@@ -44,14 +20,15 @@
 
 
                     @if (URL::current() == route('event.register', 1))
-                    <a href="{{ URL::to('/') }}" class="d-none">
-                        <i class="fa fa-home" aria-hidden="true"></i>
-                        <span>Home</span>
-                    </a>
+                        <a href="{{ URL::to('/') }}" class="d-none">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                            <span>Home</span>
+                        </a>
                     @else
-                    <a href="{{ route('event.register', 1) }}" class="register text-dark d-none d-md-block">
-                        Register Now
-                    </a>
+                        <a href="{{ route('event.register', 1) }}"
+                            class="btn btn-navy text-danger text-dark d-none d-md-block">
+                            <i class="fa fa-registered" aria-hidden="true"></i> Register Now
+                        </a>
                     @endif
 
 
