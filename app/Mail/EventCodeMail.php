db2b9242907ea91d4f5989290a0c1febf72fe85c
kesharg <70738855+kesharg@users.noreply.github.com>
2024-08-13 16:32:18 +0545

    Gatepass layouts and email

--- app/Mail/EventCodeMail.php
+++ app/Mail/EventCodeMail.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class EventCodeMail extends Mailable
 {
@@ -13,30 +14,62 @@ class EventCodeMail extends Mailable
 
     public $registration;
     public $qrToken;
+    public $qrGatePass;
 
-    public function __construct($registration, $qrToken)
+    public function __construct($registration)
     {
         $this->registration = $registration;
-        $this->qrToken = $qrToken;
+        $this->qrToken = $this->generateQrCode();
+        $this->qrGatePass = $this->generateQrGatepass();
     }
+
+public function generateQrCode()
+{
+    // Create the QR code
+    $qrCodeData = QrCode::format('png')->size(300)->generate($this->registration->event_token);
+
+    // Define the file path
+    $filePath = 'qrcodes/' . $this->registration->event_token . '.png';
+
+    // Save the QR code to storage
+    Storage::disk('public')->put($filePath, $qrCodeData);
+
+    return $filePath;
+}
+public function generateQrGatepass()
+{
+    // Create the QR code
+    $url = route('eventregistrations.gatepass.details', ['event_token' => $this->registration->event_token]);
+
+        // Create the QR code for the details page
+        $qrCodeData = QrCode::format('png')->size(300)->generate($url);
+
+        // Define the file path
+        $filePath = 'gatepass/' . $this->registration->event_token . '.png';
+
+        // Save the QR code to storage
+        Storage::disk('public')->put($filePath, $qrCodeData);
+
+        return $filePath;
+}
 
     public function build()
     {
-        // Define the file path for the PNG QR code
-        $pngPath = $this->qrToken;
+        $filePathToken = 'qrcodes/' . $this->registration->event_token . '.png';
+        $filePathGatePass = 'gatepass/' . $this->registration->event_token . '.png';
 
-        // Get the absolute path to the QR code file
-        $absolutePath = Storage::disk('public')->path($pngPath);
-
-        // Attach the PNG QR code to the email
         return $this->markdown('emails.event_code')
                     ->with([
                         'registration' => $this->registration,
-                    ])
-                    ->attach($absolutePath, [
-                        'as' => 'qr_code.png',
+                        'qrToken' => $this->qrToken,
+                        'qrGatePass' => $this->qrGatePass,
+
+                    ])->attach(storage_path('app/public/' . $filePathToken), [
+                        'as' => 'qrcode.png',
                         'mime' => 'image/png',
-                    ]);
+                    ])->attach(storage_path('app/public/' . $filePathGatePass), [
+                        'as' => 'gatepass.png',
+                        'mime' => 'image/png',
+                    ]);;
     }
 }
-
---
+++ resources/views/gatepass-detail.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.gatepass')
+
+@section('title', 'Gate Pass')
+
+@section('content')
+
+<!-- end header section -->
+<!-- slider section -->
+<section class="message-section fixed-vh d-flex align-items-center">
+    <div class="container text-center">
+
+        <div class="card">
+            <div class="card-header fw-bold text-uppercase p-2 p-md-3">Event Details</div>
+            <div class="card-body p-2 p-md-3">
+                <div class="row mb-2 mb-md-4">
+                    <p class="banner-title small-text fw-bold mb-1 mb-md-2">{!! $event->banner_text !!}</p>
+
+                    <h3 class="banner-title main">{{$event->name}}</h3>
+
+                    <p class="mb-1">
+                        <strong>Date:</strong>
+                        {{ \Carbon\Carbon::parse($event->start_date)->format('d') }}
+                        - {{ \Carbon\Carbon::parse($event->end_date)->format('d F Y') }}
+                    </p>
+                    <p class="mb-2 mb-md-4">
+                        <strong>Venue:</strong>
+                        {{$event->location}}
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header fw-bold text-uppercase p-2 p-md-3">Your Details</div>
+            <div class="card-body p-2 p-md-3">
+                <div class="row mb-2 mb-md-4">
+                    <div class="form-group col-md-4">
+                        <label class="form-label" for="nmc_registration_number"><strong>NMC Number</strong> </label>:-
+                        {{$registration->nmc_registration_number}}
+                    </div>
+
+                    <div class="form-group col-md-4">
+                        <label class="form-label" for="full_name"><strong>Full Name</strong> </label>:-
+                        {{$registration->full_name}}
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label class="form-label" for="address"><strong>Address</strong> </label>:-
+                        {{$registration->address}}
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label class="form-label" for="degree"><strong>Degree</strong> </label>:-
+                        {{$registration->degree}}
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label class="form-label" for="nmc_registration_number"><strong>Email Address</strong> </label>:-
+                        {{$registration->email_address}}
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label class="form-label" for="nmc_registration_number"><strong>Phone Number</strong> </label>:-
+                        {{$registration->phone_number}}
+                    </div>
+                </div>
+                @foreach($ticketDerails as $ticket)
+                @php
+                @endphp
+                <div class="form-row mb-4">
+                    <div class="col-md-12">
+                        <div class="heading mb-2">{{$ticket->eventcategoryticket->eventcategory->title}}</div>
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Ticket</th>
+                                        <th>Early Bird (Till 20th Oct 2024)</th>
+                                        <th>Late & On-Site</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td width="50%">{{ $ticket->eventcategoryticket->title }}</td>
+                                        <td>
+                                            {{ $ticket->eventCategoryTicketPriceType->name == 'Early Bird' ? '✔' : '' }}
+                                        </td>
+                                        <td>
+                                            {{ $ticket->eventCategoryTicketPriceType->name != 'Early Bird' ? '✔' : '' }}
+
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
+                @endforeach
+            </div>
+
+        </div>
+
+
+    </div>
+</section>
+@endsection
